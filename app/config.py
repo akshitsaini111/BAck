4d7e17f5ab4ceeefbb0eb4ebce000da738e43748
AkshitSaini <akshit.saini@u-next.com>
2025-02-07 22:21:33 +0530

    Added Comments for config file

--- app/config.py
+++ app/config.py
@@ -2,11 +2,11 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+#MONGO
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 DB_NAME = os.getenv("DB_NAME", "vector_db")
 COLLECTION_NAME = os.getenv("COLLECTION_NAME", "embeddings")
-
+#AWS
 AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
