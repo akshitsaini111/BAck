21f4545e81723871dd9ba87e8a3f67815e8ecd4e
AkshitSaini <akshit.saini@u-next.com>
2025-02-08 18:01:48 +0530

    -y added

--- service.sh
+++ service.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install python3-venv
+sudo apt-get update -y
+sudo apt-get install python3-venv -y
 
 cd /home/ubuntu
 git clone https://github.com/akshitsaini111/BAck.git
@@ -10,5 +10,5 @@ cd BAck
 
 python3 -m venv venv
 source venv/bin/activate
-pip install -r requirements.txt
+pip install -r requirements.txt 
 uvicorn app.main:app --host 0.0.0.0 --port 6969
