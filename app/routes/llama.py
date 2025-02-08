eb917707e20f5898a56d9ed4804eac6d7704d19e
AkshitSaini <akshit.saini@u-next.com>
2025-02-08 17:12:39 +0530

    Change in Routres

--- app/routes/llama.py
+++ app/routes/llama.py
@@ -4,9 +4,13 @@ from app.services.llama import generate_response, get_stored_response
 
 router = APIRouter()
 
+# @router.post("/generate")
+# async def generate_llama_response(input_data: PromptInput):
+#     return await generate_response(input_data.prompt)
+
 @router.post("/generate")
-async def generate_llama_response(input_data: PromptInput):
-    return await generate_response(input_data.prompt)
+async def generate_llama_response():
+    return await generate_response()
 
 @router.get("/get/{response_id}")
 async def get_response(response_id: str):
